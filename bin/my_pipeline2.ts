efdee044201e695f6cb96a1fc9768fbdb8cfdb80
Thomas Setz <thomas@setz.de>
2022-10-02 16:55:15 +0200

    magic

--- bin/my_pipeline2.ts
+++ bin/my_pipeline2.ts
@@ -3,12 +3,14 @@ import * as cdk from 'aws-cdk-lib';
 import { MyPipelineStack } from '../lib/my_pipeline2-stack';
 
 const app = new cdk.App();
+console.log("Before Stack")
 new MyPipelineStack(app, 'MyPipelineStack', {
   env: {
     account: '928141648496',
     region: 'eu-central-1',
   }
 });
+console.log("After Stack")
 
 app.synth();
 
--- lib/my_pipeline2-stack.ts
+++ lib/my_pipeline2-stack.ts
@@ -1,12 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { CodeBuildStep, CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 
 export class MyPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const pipeline = new CodePipeline(this, 'Pipeline', {
+    
+    const muh = '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'
+
+    console.log("Befor Create muuh")
+/*    const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'MyPipeline',
       synth: new ShellStep('Synth', {
        input: CodePipelineSource.gitHub('thsetz/my_pipeline2', 'master'),
@@ -14,6 +18,34 @@ export class MyPipelineStack extends cdk.Stack {
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
+*/
+    // START NEW
+    // Set your Github username and repository name
+    const branch = 'master';
+    const gitHubUsernameRepository = 'thsetz/my_pipeline2';
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+            pipelineName: "MyCDKPipeline",
+            synth: new CodeBuildStep('SynthStep', {
+                input: CodePipelineSource.gitHub(gitHubUsernameRepository, branch, {
+                    // authentication: cdk.SecretValue.secretsManager('github-access-token-secret'),
+                    authentication: muh,
+                }),
+                installCommands: [
+                    'npm install -g aws-cdk'
+                ],
+                commands: [
+                    'npm ci',
+                    'npm run build',
+                    'npx cdk synth'
+                ]
+            })
+        });
+ 
+
+    // FIN NEW
+
+    console.log("After Create muuh")
+    
   }
 }
 
